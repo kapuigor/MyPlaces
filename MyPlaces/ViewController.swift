b8e4ace921b3580b3b4215e8bf61c77b0bee7da9
Igor Kapustin <kapui-igor@ya.ru>
2021-07-22 17:29:52 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Игорь Капустин on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
